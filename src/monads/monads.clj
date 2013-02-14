13f42eaa47d4507a8250ab09e84a85dde5f480a0
ben wolfson <wolfson@readyforzero.com>
2013-02-13 17:35:54 -0800

    just use identity

--- src/monads/monads.clj
+++ src/monads/monads.clj
@@ -244,14 +244,14 @@
     (assoc cont-m
       :monadtrans {:lift (curryfn [m c] (c (run-monad inner m)))})))
 
-(defn run-cont [m c]
-  (let [m ((run-monad cont-m m) c)]
+(defn run-cont [m]
+  (let [m ((run-monad cont-m m) identity)]
     (if (cont? m)
       (recur (.c m) (.v m))
       m)))
 
-(defn run-cont-t [m comp cont]
-  (let [comp ((run-monad m comp) cont)]
+(defn run-cont-t [m comp & [cont]]
+  (let [comp ((run-monad m comp) (or cont identity))]
     (if (cont? comp)
       (recur m (.c comp) (.v comp))
       comp)))
