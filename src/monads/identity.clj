355b653d8b3d90eb25bd3d54d62c31e8ee0d5669
ben wolfson <wolfson@gmail.com>
2013-02-24 12:59:32 -0800

    have to run this

--- src/monads/identity.clj
+++ src/monads/identity.clj
@@ -1,6 +1,6 @@
 (ns monads.identity
-  (:use [monads.core :only [defmonad]]))
+  (:use [monads.core :only [defmonad run-monad]]))
 
 (defmonad identity-m
-  :bind (fn [m f] (f m))
+  :bind (fn [m f] (run-monad identity-m (f m)))
   :return identity)
