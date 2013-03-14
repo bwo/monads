29aeb3d6b14b97e18305f30e6cd042a79045bd41
ben wolfson <wolfson@gmail.com>
2013-03-13 20:19:06 -0700

    bump

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [the/parsatron "0.0.4"]
-                 [bwo/macroparser "0.0.1"]
+                 [bwo/macroparser "0.0.6"]
                  [readyforzero/babbage "1.0.1"]
                  [expectations "1.4.18"]])
