2545e43947b7462e4fad846797f5bff5c723487a
ben wolfson <wolfson@gmail.com>
2013-03-18 22:13:25 -0700

    list-t

--- src/monads/list.clj
+++ src/monads/list.clj
@@ -1,5 +1,6 @@
 (ns monads.list
-  (:require [monads.core :refer :all]))
+  (:require [monads.core :refer :all]
+            [monads.util :as u]))
 
 
 (defn mcat [f xs]
@@ -8,7 +9,6 @@
      nil
      (concat (f (first xs)) (mcat f (rest xs))))))
 
-;; list-t is not always a correct transformer. Omitted.
 (defmonad list-m
   :return list
   :bind (fn [m f]
@@ -18,4 +18,28 @@
                        (concat (run-monad list-m (first leftright))
                                (run-monad list-m (second leftright))))})
 
+;; note that this is not always a monad.
+(declare list-t)
+(defn list-t* [inner]
+  (let [i-return (:return inner)]
+    (monad
+     :return (comp i-return list)
+     :bind (fn [m f]
+             (run-mdo inner
+                      a <- (run-monad (list-t inner) m)
+                      b <- (u/sequence-m (map (comp (partial run-monad (list-t inner)) f) a))
+                      (return (apply concat b))))
+     :monadplus {:mzero (i-return ())
+                 :mplus (fn [lr]
+                          (run-mdo inner
+                                   a <- (run-monad (list-t inner) (first lr))
+                                   b <- (run-monad (list-t inner) (second lr))
+                                   (return (concat a b))))}
+     :monadtrans {:list (fn [m]
+                          (run-mdo inner
+                                   a <- (run-monad (list-t inner) m)
+                                   (return (list a))))})))
+(def list-t (memoize list-t*))
+
 (def m list-m)
+(def t list-t)
