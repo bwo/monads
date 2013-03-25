5b92a9b366236365b10cf88d04cf9101f278a2ec
ben wolfson <wolfson@gmail.com>
2013-03-24 21:50:08 -0700

    de-snapshot version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject bwo/monads "0.1.0-SNAPSHOT"
-  :description "YA monad library"
+(defproject bwo/monads "0.1.0"
+  :description "YA monad library for clojure"
   :url "http://github.com/bwo/monads"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
