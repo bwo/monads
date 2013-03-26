681d1a3500e705c164cc576a4880012c1e8a0703
ben wolfson <wolfson@gmail.com>
2013-03-25 21:31:41 -0700

    more monadplus test

--- test/monads/test_mplus.clj
+++ test/monads/test_mplus.clj
@@ -27,8 +27,25 @@
                (run-monad e/error-m)
                t/from-right))
 
+(expect [5 6] (run-monad l/list-m (mplus (return 5) (return 6))))
+
 (expect [5 6] (r/run-reader-t (r/reader-t l/list-m) (mplus ask (asks inc)) 5))
 
 (expect 5 (t/from-just (r/run-reader-t (r/reader-t m/maybe-m) (mplus ask (asks inc)) 5)))
+
+(expect 5 (t/from-just (r/run-reader (run-monad (m/maybe-t r/reader-m) (mplus (lift (asks inc))
+                                                                              mzero)) 4)))
+
+(expect 5 (t/from-just (r/run-reader (run-monad (m/maybe-t r/reader-m)
+                                                (mplus mzero
+                                                       (lift (asks inc)))) 4)))
+
+(expect [5 6] (r/run-reader-t
+               (r/t l/m) (s/eval-state-t (s/t (r/t l/m))
+                                         (mplus (modify dec)
+                                                (lift (local inc
+                                                             (mdo x <- ask
+                                                                  (return (+ x 2))))))
+                                         6) 3))
 
 (expect 6 (t/from-just (r/run-reader-t (r/reader-t m/maybe-m) (mplus mzero (asks inc)) 5)))
