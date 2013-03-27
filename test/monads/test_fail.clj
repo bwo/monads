18a28dc658b68bb8d1d05cf463514dce02a2f653
ben wolfson <wolfson@gmail.com>
2013-03-26 21:59:22 -0700

    test fail

---
+++ test/monads/test_fail.clj
@@ -0,0 +1,32 @@
+(ns monads.test-fail
+  (:require [monads.maybe :as m]
+            [monads.error :as e]
+            [monads.state :as s]
+            [monads.writer :as w]
+            [monads.reader :as r]
+            [monads.types :as t]
+            [monads.list :as l])
+  (:use expectations
+        monads.core))
+
+(defn safe-div [num denom]
+  (if (== 0 denom)
+    (mfail "zero denominator!")
+    (return (/ num denom))))
+
+(expect nil (run-monad m/m (safe-div 4 0)))
+(expect nil (run-monad l/m (safe-div 4 0)))
+(expect "zero denominator!" (t/from-left (run-monad e/m (safe-div 5 0))))
+
+(expect "zero denominator!" (t/from-left
+                             (s/run-state-t (s/t e/m)
+                                            (mdo x <- get-state
+                                                 (lift (safe-div 2 x)))
+                                            0)))
+
+(expect [2 1]
+        (seq (t/from-right
+              (s/run-state-t (s/t e/m)
+                             (mdo x <- get-state
+                                  (lift (safe-div 2 x)))
+                             1))))
