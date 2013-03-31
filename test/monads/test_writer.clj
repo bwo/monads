6ceafa665f6b0c99e25933fb734a0299d8ee966c
ben wolfson <wolfson@gmail.com>
2013-03-31 13:35:08 -0700

    writer-t

--- test/monads/test_writer.clj
+++ test/monads/test_writer.clj
@@ -1,5 +1,7 @@
 (ns monads.test-writer
-  (:require [monads.writer :as w])
+  (:require [monads.writer :as w]
+            [monads.error :as e]
+            [monads.types :as t])
   (:use monads.core
         expectations))
 
@@ -11,6 +13,13 @@
                                                       (return 1)))
                                         (return y)))))
 
+(expect [[1 [2]] [3 2]]
+        (seq (t/from-right (run-monad (w/t e/m) (mdo x <- (return 3)
+                                                      (tell [3])
+                                                      y <- (listen (mdo
+                                                                    (tell [2])
+                                                                    (return 1)))
+                                                      (return y))))))
 (expect [1 [3 2]]
         (seq (run-monad w/writer-m (mdo x <- (return 3)
                                         (tell [3])
@@ -21,6 +30,15 @@
                                         (return y)))))
 
 (expect [1 [3 2]]
+        (seq (t/from-right (run-monad (w/t e/m) (mdo x <- (return 3)
+                                                     (tell [3])
+                                                     y <- (pass (mdo
+                                                                 (tell [1])
+                                                                 (tell [2])
+                                                                 (return [1 rest])))
+                                                     (return y))))))
+
+(expect [1 [3 2]]
         (seq (run-monad w/writer-m (mdo x <- (return 3)
                                         (tell [3])
                                         y <- (censor rest(mdo
@@ -28,3 +46,12 @@
                                                           (tell [2])
                                                           (return 1)))
                                         (return y)))))
+
+(expect [1 [3 2]]
+        (seq (t/from-right (run-monad (w/t e/m) (mdo x <- (return 3)
+                                                     (tell [3])
+                                                     y <- (censor rest(mdo
+                                                                       (tell [1])
+                                                                       (tell [2])
+                                                                       (return 1)))
+                                                     (return y))))))
