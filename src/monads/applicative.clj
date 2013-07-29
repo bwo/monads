7639e92b0704e3eb698dcbda89e7ea43cc29e477
ben wolfson <wolfson@gmail.com>
2013-07-28 21:42:02 -0700

    more ap

--- src/monads/applicative.clj
+++ src/monads/applicative.clj
@@ -1,7 +1,9 @@
 (ns monads.applicative
-  (:require [monads.types :as types]
+  (:require [babbage.monoid :as m]
+            [monads.types :as types]
             [monads.util :as u]
-            [monads.core :as core])
+            [monads.core :as core]
+            [clojure.algo.generic.functor :as f])
   (:import [monads.types Just Either Mplus Returned Bind Return]))
 
 (set! *warn-on-reflection* true)
@@ -28,8 +30,30 @@
     (Return. (.f o))
     o))
 
-(defn fapply-mon [v f]
+(defn- fapply-mon [v f]
   (u/ap (pure->mon f) v))
+
+(defn- fapply-lazyseq [me f]
+  (if-let [fs (types/if-instance Pure f
+                [(.f f)]
+                (seq f))]
+    (u/mcat (fn [f] (map f me)) fs)
+    nil))
+
+(deftype Const [t])
+
+(defn get-const [^Const t] (.t t))
+
+(defmethod f/fmap Const
+  [f v] v)
+
+(deftype Id [i])
+
+(defn get-id [^Id i] (.i i))
+
+(defmethod f/fmap Id
+  [f ^Id v]
+  (Id. (f (.i v))))
 
 (extend-protocol Applicative
   Just
@@ -43,8 +67,8 @@
   (fapply [me o]
     (if (types/right? me)
       (if-let [f (types/cond-instance o
-                       Pure (.f o)
-                       Either (types/either (constantly nil) identity o))]
+                     Pure (.f o)
+                     Either (types/either (constantly nil) identity o))]
         (types/right (f (types/from-right me)))
         o)
       me))
@@ -53,4 +77,41 @@
   (fapply [me f] (fapply-mon me f))
 
   Bind
-  (fapply [me f] (fapply-mon me f)))
+  (fapply [me f] (fapply-mon me f))
+
+  Returned
+  (fapply [me f] (fapply-mon me f))
+
+  Mplus
+  (fapply [me f] (fapply-mon me f))
+
+  clojure.lang.IPersistentVector
+  (fapply [me f] (if-let [fs (types/if-instance Pure f
+                               [(.f f)]
+                               (seq f))]
+                   (vec (u/mcat (fn [f] (mapv f me)) fs))
+                   []))
+
+  clojure.lang.IPersistentList
+  (fapply [me f] (fapply-lazyseq me f))
+
+  clojure.lang.LazySeq
+  (fapply [me f] (fapply-lazyseq me f))
+
+  clojure.lang.PersistentVector$ChunkedSeq
+  (fapply [me f] (fapply-lazyseq me f))
+
+  nil
+  (fapply [me f] me)
+
+  Const
+  (fapply [me f] (let [f (types/cond-instance f
+                             Pure nil
+                             Const (.t f))]
+                   (m/<> f (.t me))))
+
+  Id
+  (fapply [me f] (if-let [f (types/cond-instance f
+                                Pure (.f f)
+                                Id (.i f))]
+                   (Id. (f (.i me))))))
