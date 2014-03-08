71582770ad9e2f17718ba53f5bdbe87d71ebfd2b
ben wolfson <wolfson@readyforzero.com>
2014-03-07 17:38:40 -0800

    version upgrades

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/bwo/monads"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/algo.generic "0.1.1"]
                  [the/parsatron "0.0.4"]
-                 [bwo/macroparser "0.0.6"]
+                 [bwo/macroparser "0.0.7b"]
                  [readyforzero/babbage "1.0.1"]
                  [expectations "1.4.18"]])
